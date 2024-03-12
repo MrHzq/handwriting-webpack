45d18cb0511c584bf638cab8cf5166e51a75e3ee
黄智强 <huangzq@dustess.com>
2024-03-12 12:17:43 +0800

    feat: webpack.js编码：将上述代码聚合到一个方法内 - getModuleInfo

--- webpack.js
+++ webpack.js
@@ -2,7 +2,8 @@
  * 功能设计：
  * 1. 找到主入口，即 src/index.js 文件，然后加载进来(getFileInfo(path) -> fileContent)
  * 2. 解析主入口的内容(parseFile(fileContent))，找到所有依赖，形成依赖关系(createDependencyMap(AST) -> dependencyMap)
- * 3. 根据依赖关系，加载对应文件，复用 getFileInfo 方法
+ * 3. 在将 AST 转换成低版本的 JS 代码，(generateCode(AST))
+ * 4. 基于依赖关系图，去加载对应的所有文件
  */
 
 // 主入口路径变量，目前写死
@@ -14,66 +15,90 @@ const path = require("path");
 // fs 模块，读取文件内容
 const fs = require("fs");
 
-/**
- * 获取文件信息
- *
- * @param path 文件路径
- * @returns 返回文件内容
- */
-function getFileInfo(path) {
-  // 使用 fs.readFileSync 方法同步读取文件内容
-  return fs.readFileSync(path, "utf-8");
-}
-
-const entryFileContent = getFileInfo(entry);
-
 // @babel/parser 解析文件内容
 const parser = require("@babel/parser");
 
-/**
- * 解析文件内容并返回抽象语法树（AST）
- *
- * @param fileContent 文件内容
- * @returns 抽象语法树（AST）
- */
-function parseFile(fileContent) {
-  // 解析文件内容，生成抽象语法树（AST）
-  const ast = parser.parse(fileContent, {
-    sourceType: "module", // 要解析的模块是 ESM
-  });
-  // 返回抽象语法树（AST）
-  return ast;
-}
-
-const entryFileContentAST = parseFile(entryFileContent);
-
 const traverse = require("@babel/traverse").default;
-/**
- * 创建依赖关系图
- *
- * @param ast 抽象语法树
- * @returns 依赖关系图
- */
-function createDependencyMap(ast) {
-  // 创建依赖关系图
-  const dependencyMap = {};
 
-  // 遍历抽象语法树（AST）
-  traverse(ast, {
-    ImportDeclaration({ node }) {
-      const { value } = node.source; // 从 AST 中获取到导入的相对文件路径
+const babelCore = require("@babel/core");
 
-      const dirname = path.dirname(entry); // 获取存放主入口文件的文件名
+function getModuleInfo(_path) {
+  /**
+   * 获取文件信息
+   *
+   * @param path 文件路径
+   * @returns 返回文件内容
+   */
+  function getFileInfo(path) {
+    // 使用 fs.readFileSync 方法同步读取文件内容
+    return fs.readFileSync(path, "utf-8");
+  }
 
-      const abspath = "./" + path.join(dirname, value); // 拼接出每个导入文件的绝对路径
+  /**
+   * 解析文件内容并返回抽象语法树（AST）
+   *
+   * @param fileContent 文件内容
+   * @returns 抽象语法树（AST）
+   */
+  function parseFile(fileContent) {
+    // 解析文件内容，生成抽象语法树（AST）
+    const ast = parser.parse(fileContent, {
+      sourceType: "module", // 要解析的模块是 ESM
+    });
+    // 返回抽象语法树（AST）
+    return ast;
+  }
 
-      dependencyMap[value] = abspath; // 添加到依赖关系图
-    },
-  });
+  /**
+   * 创建依赖关系图
+   *
+   * @param ast 抽象语法树
+   * @returns 依赖关系图
+   */
+  function createDependencyMap(ast) {
+    // 创建依赖关系图
+    const dependencyMap = {};
 
-  console.log("[ dependencyMap ] >", dependencyMap);
+    // 遍历抽象语法树（AST）
+    traverse(ast, {
+      ImportDeclaration({ node }) {
+        const { value } = node.source; // 从 AST 中获取到导入的相对文件路径
 
-  return dependencyMap;
+        const dirname = path.dirname(entry); // 获取存放主入口文件的文件名
+
+        const abspath = "./" + path.join(dirname, value); // 拼接出每个导入文件的绝对路径
+
+        dependencyMap[value] = abspath; // 添加到依赖关系图
+      },
+    });
+
+    return dependencyMap;
+  }
+
+  /**
+   * 生成代码
+   *
+   * @param ast AST 对象
+   * @returns 返回生成的代码字符串
+   */
+  function generateCode(ast) {
+    // 使用 Babel 将抽象语法树（AST）转换为可执行的 JavaScript 代码
+    const { code } = babelCore.transformFromAst(ast, null, {
+      presets: ["@babel/preset-env"], // 指定转译的语法
+    });
+
+    // 返回生成的代码
+    return code;
+  }
+
+  const _pathFileContent = getFileInfo(_path);
+  const _pathFileContentAST = parseFile(_pathFileContent);
+  const _pathFileDepsMap = createDependencyMap(_pathFileContentAST);
+  const _pathFileCode = generateCode(_pathFileContentAST);
+
+  return { path: _path, deps: _pathFileDepsMap, code: _pathFileCode };
 }
 
-createDependencyMap(entryFileContentAST);
+const entryModuleInfo = getModuleInfo(entry);
+
+console.log("[ entryModuleInfo ] >", entryModuleInfo);
